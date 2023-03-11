f810c745b63bbd1bd6eb8104234518c679a86e22
BattlestationX2\MJM <MilesJMaiden@Gmail.com>
2023-03-11 08:22:08 +0000

    Added math function for enemy to throw their gun towards player on death

--- SuperChill/Assets/Enemy.cs
+++ SuperChill/Assets/Enemy.cs
@@ -15,12 +15,16 @@ public class Enemy : MonoBehaviour
 
     public float stopDistance = 6f;
 
+    private Quaternion localRotationGun;
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>(); 
         SetupRagdoll();
+
+        localRotationGun = gun.bulletOrigin.transform.localRotation;
     }
 
     // Update is called once per frame
@@ -34,6 +38,32 @@ public class Enemy : MonoBehaviour
             agent.isStopped = true;
             animator.SetBool("Shoot", true);
         }
+    }
+
+    public void ThrowGun()
+    {
+        gun.bulletOrigin.localRotation = localRotationGun;
+
+        gun.transform.parent = null;
+        //Maths Physics Method
+        Rigidbody rb = gun.GetComponent<Rigidbody>();
+        rb.velocity = BallisticVelocityVector(gun.transform.position, playerHead.position, 45f);
+        rb.angularVelocity = Vector3.zero;
+    }
+    
+    Vector3 BallisticVelocityVector(Vector3 source, Vector3 target, float angle)
+    {
+        Vector3 direction = target - source;
+        float h = direction.y;
+        direction.y = 0;
+        float distance = direction.magnitude;
+        float a = angle * Mathf.Deg2Rad;
+        direction.y = distance * Mathf.Tan(a);
+        distance += h / Mathf.Tan(a);
+
+        //calculate velocity
+        float velocity = Mathf.Sqrt(distance * Physics.gravity.magnitude / Mathf.Sin(2 * a));
+        return velocity * direction;
     }
 
     public void ShootEnemy()
@@ -71,5 +101,12 @@ public class Enemy : MonoBehaviour
                 rb.AddExplosionForce(1000, hitPosition, 0.3f);
             }
         }
+
+
+        ThrowGun();
+        animator.enabled= false;
+        agent.enabled= false;
+        this.enabled= false;
+
     }
 }
