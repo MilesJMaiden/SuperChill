47422b64fadcdb8cf2091649983da758ec6e0681
BattlestationX2\MJM <MilesJMaiden@Gmail.com>
2023-03-11 06:47:35 +0000

    Added death triggers for enemies

--- SuperChill/Assets/Enemy.cs
+++ SuperChill/Assets/Enemy.cs
@@ -7,12 +7,42 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetupRagdoll();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SetupRagdoll()
+    {
+        //loop through all limbs
+        foreach (var item in GetComponentsInChildren<Rigidbody>())
+        {
+            item.isKinematic = true;
+        }
+    }
+
+    public void Dead(Vector3 hitPosition)
+    {
+        //loop through all limbs
+        foreach (var item in GetComponentsInChildren<Rigidbody>())
+        {
+            item.isKinematic = true;
+        }
+
+        //Gets all body components within range of hitposition 
+        foreach (var item in Physics.OverlapSphere(hitPosition, 0.3f))
+        {
+            Rigidbody rb = item.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+
+                rb.AddExplosionForce(1000, hitPosition, 0.3f);
+            }
+        }
     }
 }
