77b2feda5973d09459ce5f5b4d232a22ce4099ad
BattlestationX2\MJM <MilesJMaiden@Gmail.com>
2023-03-11 07:05:03 +0000

    NavMesh baked, Enemy AI will move to player position

--- SuperChill/Assets/Enemy.cs
+++ SuperChill/Assets/Enemy.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+
+    private NavMeshAgent agent;
+    public Transform playerTarget;
+
     // Start is called before the first frame update
     void Start()
     {
+        agent = GetComponent<NavMeshAgent>();
         SetupRagdoll();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(playerTarget.position);
     }
 
     public void SetupRagdoll()
